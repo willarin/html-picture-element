1b6da407bc6414d0d7b30dd1deef078d1cf43b4c
ramil <ramil@almeydahost.com>
2018-05-14 16:16:25 +0300

    Class definition fixes

--- src/HtmlPicture.php
+++ src/HtmlPicture.php
@@ -1,11 +1,15 @@
 <?php
 
-namespace app\modules\common\components;
+namespace deitsolutions\htmlpicture\src;
 
 use deitsolutions\versioning\src\FileVersioning;
 
 class HtmlPicture
 {
+    /**
+     * @var string src attribute of image tag
+     */
+    public $src = '';
     /**
      * @var array image tag attributes
      */
@@ -17,12 +21,21 @@ class HtmlPicture
 
     /**
      * get picture element
-     * @param $src
+     * @param $config
      * @return string
      */
-    public function get($src)
+    public function get($config)
     {
-        $srcParts = pathinfo($src);
+        //populate properties with configuration array
+        if (is_array($config)) {
+            foreach ($config as $key => $value) {
+                if (isset($this->{$key})) {
+                    $this->{$key} = $value;
+                }
+            }
+        }
+
+        $srcParts = pathinfo($this->src);
 
         //collect attributes into string
         $attributesString = '';
@@ -33,13 +46,13 @@ class HtmlPicture
         //form picture tag
         $html = '<picture>';
         foreach ($this->sourceTypes as $type) {
-            $sourceSrc = str_replace('.' . $srcParts['extension'], '.' . $type, $src);
+            $sourceSrc = str_replace('.' . $srcParts['extension'], '.' . $type, $this->src);
             if (file_exists(__DIR__ . $sourceSrc)) {
                 $html .= '<source srcset = "' . FileVersioning::set($sourceSrc) . '" type = "image/' . $type . '">';
             }
         }
 
-        $html .= '<img src="' . FileVersioning::set($src) . '" ' . $attributesString . '>' . '</picture>';
+        $html .= '<img src="' . FileVersioning::set($this->src) . '" ' . $attributesString . '>' . '</picture>';
 
         return $html;
     }
